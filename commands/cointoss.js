2ca6d1cf8e968a58c866ae9bb3a1af8ae80c11df
themightyzek <zekromfreak@gmail.com>
2018-06-17 14:58:34 +0200

    added cointoss.js

---
+++ commands/cointoss.js
@@ -0,0 +1,12 @@
+module.exports = {
+	name: 'kick',
+	description: 'End an argument the honorable way.',
+	guildOnly: true,
+	execute(message, args) {
+        //Timo ist gay      
+        
+		const taggedUser = message.mentions.users.first();
+
+		message.channel.send(`You wanted to kick: ${taggedUser.username}`);
+	},
+};
