391858fd931b2575bfe9f902d1c858bc4b6a3d0d
TimoGa <e-mail.timo.g@gmx.de>
2018-06-17 15:18:39 +0200

    react

--- commands/react.js
+++ commands/react.js
@@ -2,6 +2,20 @@ module.exports = {
 	name: 'react',
 	description: 'Reaktion!',
 	execute(message) {
-		message.react('😄');
+        var chance = Math.floor(Math.random() * 10) + 1;
+
+        switch(chance) {
+            case 1: message.react('🤔');
+            case 2: message.react('🤗');
+            case 3: message.react('😱');
+            case 4: message.react('😉');
+            case 5: message.react('🤤');
+            case 6: message.react('😰');
+            case 7: message.react('😡');
+            case 8: message.react('😏');
+            case 9: message.react('😒');
+            case 10: message.react('😩');
+            default: break;
+        }
 	},
 };
