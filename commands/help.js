603ac86d1bbecf409494f72e02e3f44e152f1877
TimoGa <e-mail.timo.g@gmx.de>
2018-06-18 21:45:53 +0200

    schönerer help-comand

--- commands/help.js
+++ commands/help.js
@@ -11,8 +11,8 @@ module.exports = {
 		const { commands } = message.client;
 
 		if (!args.length) {
-			data.push('Here\'s a list of all my commands:');
-			data.push(commands.map(command => command.name).join(', '));
+			data.push('Here\'s a list of all my commands:\n');
+			data.push('>	' + commands.map(command => command.name).join('\n>	'));
 			data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
 
 			return message.author.send(data, { split: true })
