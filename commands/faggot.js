a28ed4db1e4a6600c4d7425e1b294de7a4b45169
themightyzek <zekromfreak@gmail.com>
2018-06-18 18:39:35 +0200

    Minor performance tweaks  Please enter the commit message for your changes. Lines starting

--- commands/faggot.js
+++ commands/faggot.js
@@ -3,11 +3,11 @@
         name: 'faggot',
         description: 'Faggot!',
         execute(message) {
-            if(message.author.username == "Timo"){
+            if(message.author.username == "Zek"){
                 message.reply("ist ein fähiger Programmierer!");
                 return;
             }
-            if(message.author.username != "Timo"){
+            if(message.author.username != "Zek"){
                 message.reply("ist ein Faggot!");
             }
         },
