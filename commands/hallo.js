a03e9da0a9213f111423fff62ea2a483f0527905
TimoGa <e-mail.timo.g@gmx.de>
2018-06-18 19:06:50 +0200

    youtube

--- commands/hallo.js
+++ commands/hallo.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'hallo',
 	description: 'Hallo!',
 	execute(message) {
-		message.channel.send("Hallo, Marcel B'avis, 1&1&1").then(sentMessage => {
+			message.channel.send("Hallo, Marcel B'avis, 1&1&1").then(sentMessage => {
 			sentMessage.react('☎');
 			sentMessage.react('🌐');
 		});
--- commands/sing.js
+++ commands/sing.js
@@ -4,21 +4,19 @@ module.exports = {
 	name: 'sing',
 	description: 'Sing a song!',
 	execute(message) {
-        client.on('message', message => {
-                if (message.channel.type !== 'text') return;
+        if (message.channel.type !== 'text') return;
 
-                const { voiceChannel } = message.member;
+        const { voiceChannel } = message.member;
 
-                if (!voiceChannel) {
-                    return message.reply('please join a voice channel first!');
-                }
+        if (!voiceChannel) {
+            return message.reply('please join a voice channel first!');
+        }
 
-                voiceChannel.join().then(connection => {
-			        const stream = ytdl('https://www.youtube.com/watch?v=D9LxMut3TMM', { filter: 'audioonly' });
-                    const dispatcher = connection.playStream(stream);
+        voiceChannel.join().then(connection => {
+			const stream = ytdl('https://www.youtube.com/watch?v=D9LxMut3TMM', { filter: 'audioonly' });
+            const dispatcher = connection.playStream(stream);
 
-                    dispatcher.on('end', () => voiceChannel.leave());
-                });
+            dispatcher.on('end', () => voiceChannel.leave());
         });
     },
 };
