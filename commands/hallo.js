783254cc09d8b28d03b7b991088fa62f34493930
TimoGa <e-mail.timo.g@gmx.de>
2018-06-19 10:12:51 +0200

    wlan :p

--- commands/hallo.js
+++ commands/hallo.js
@@ -3,8 +3,9 @@ module.exports = {
 	description: 'Begrüßung',
 	execute(message) {
 			message.channel.send("Hallo, Marcel B'avis, 1&1&1").then(sentMessage => {
-			sentMessage.react('☎');
-			sentMessage.react('🌐');
+			sentMessage.react('📶').then(() => 
+			sentMessage.react('☎')).then(() =>
+			sentMessage.react('🌐'));
 		});
 	},
 };
