9dd60903a2e40f629883bc60dba91ad94c4b5a4a
TimoGa <e-mail.timo.g@gmx.de>
2018-06-19 10:26:03 +0200

    hallo - sound

--- commands/hallo.js
+++ commands/hallo.js
@@ -1,11 +1,24 @@
+const ytdl = require('ytdl-core');
+
 module.exports = {
 	name: 'hallo',
 	description: 'Begrüßung',
 	execute(message) {
-			message.channel.send("Hallo, Marcel B'avis, 1&1&1").then(sentMessage => {
-			sentMessage.react('📶').then(() => 
-			sentMessage.react('☎')).then(() =>
-			sentMessage.react('🌐'));
+		/*if (message.channel.type !== 'text') return;
+
+        const { voiceChannel } = message.member;
+
+        voiceChannel.join().then(connection => {
+            const stream = ytdl('https://www.youtube.com/watch?v=X8ti37Bl4qE', { filter: 'audioonly' });
+            const dispatcher = connection.playStream(stream);
+
+            //dispatcher.on('end', () => voiceChannel.leave());
+        });*/
+
+		message.channel.send("Hallo, Marcel B'avis, 1&1&1").then(sentMessage => {
+		sentMessage.react('📶').then(() => 
+		sentMessage.react('☎')).then(() =>
+		sentMessage.react('🌐'));
 		});
 	},
 };
